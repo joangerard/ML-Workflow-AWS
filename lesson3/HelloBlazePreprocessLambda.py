e121e03b8f6927983384e4f23d329aa4e59b9e3b
Joan Gerard <joan.sebastian.gerard@gmail.com>
2023-06-25 17:01:44 +0000

    lambda functions test

--- lesson3/HelloBlazePreprocessLambda.py
+++ lesson3/HelloBlazePreprocessLambda.py
@@ -5,8 +5,8 @@ import os
 import boto3
 from botocore.exceptions import ClientError
 
-BUCKET_NAME = 'FILL_THIS_IN'
-PREFIX = 'FILL_THIS_IN'
+BUCKET_NAME = 'sagemaker-studio-08t3d9ufupqo'
+PREFIX = 'l3e2'
 
 # Function below unzips the archive to the local directory. 
 
